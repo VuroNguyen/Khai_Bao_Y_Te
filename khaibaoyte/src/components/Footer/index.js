c409f2cdcb54d5ed3c289589e9a7a0c6f8b16fc9
nhatlam1995 <nhatlam1695@gmail.com>
2021-07-08 03:10:14 +0700

    Base
    _ Route, configRoute
    _ Header
    _ Footer
    _ Login
    _ LoginForm

---
+++ khaibaoyte/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+
+function Footer() {
+    return (
+        <div className="footer-bottom">
+            Copyright &copy;{new Date().getFullYear()} FPT Information System - All Rights Reserved
+        </div>
+    )
+}
+
+export default Footer
