cc57b043d59566a2ce239baf9835907161afada1
nhatlam1995 <nhatlam1695@gmail.com>
2021-07-11 01:07:13 +0700

    axios config

---
+++ khaibaoyte/src/api/APIurl.js
@@ -0,0 +1,4 @@
+export const LOGIN_API = '/api/login/';
+export const REGISTER_API = '/api/register/';
+export const ACTIVATION_API = '/api/activation';
+export const GET_RESPONSE_API = '/api/report/';
