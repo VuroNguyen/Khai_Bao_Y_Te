731d26d81d145a3afb9911ca2d64ee77e905c00a
VuroNguyen <46697043+VuroNguyen@users.noreply.github.com>
2021-07-14 18:48:09 +0700

    [BE][LongNH69] upload file
    upload file in enterprise register api

--- server/routes/enterprise.js
+++ server/routes/enterprise.js
@@ -2,24 +2,37 @@ require('dotenv').config()
 const express = require('express')
 const router = express.Router()
 const jwt = require('jsonwebtoken')
+const multer = require('multer')
 
 const verifyEnterpriseToken = require('../middleware/enterpriseAuth')
 const Enterprise = require('../models/Enterprise')
 const User = require('../models/User')
 const sendMail = require('./sendMail')
 
-router.post('/register', async(req, res) => {
-    
-    const {name, email, address, MST, document} = req.body
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        const originalname = file.originalname;
+        const extension = originalname.split(".");
+        filename = Date.now() + '.' + extension[extension.length - 1];
+        cb(null, filename);
+    }
+});
 
-    if(!name || !email || !address || !document || !document)
-    return res.status(400).json({success: false, message: 'Có ô chưa nhập'})
+router.post('/register', multer({ storage: storage}).single('document'), async (req, res) => {
 
-    if(!validateEmail(email))
-    return res.status(400).json({success: false, message: 'Xin hãy nhập mail khác'})
-    
-    const enterprise = await Enterprise.findOne({email})
-    if(enterprise) return res.status(400).json({success: false, message: 'Email đã tồn tại'})
+    const { name, email, address, MST } = req.body
+
+    if (!name || !email || !address || !MST)
+        return res.status(400).json({ success: false, message: 'Có ô chưa nhập' })
+
+    // if (!validateEmail(email))
+    //     return res.status(400).json({ success: false, message: 'Xin hãy nhập mail khác' })
+
+    const enterprise = await Enterprise.findOne({ email })
+    if (enterprise) return res.status(400).json({ success: false, message: 'Email đã tồn tại' })
 
     try {
         const newEnterprise = new Enterprise({
@@ -27,38 +40,38 @@ router.post('/register', async(req, res) => {
             email,
             address,
             MST,
-            document
+            document: req.file.path,
         })
 
         await newEnterprise.save()
 
-        const accessToken = jwt.sign({enterpriseId: newEnterprise._id, name, email: newEnterprise.email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '30m'})
+        const accessToken = jwt.sign({ enterpriseId: newEnterprise._id, name, email: newEnterprise.email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30m' })
 
         const url = `http://localhost:3000/report`
 
         sendMail(email, url)
 
-        res.json({success: true, message: 'Nhận email thành công', accessToken, enterpriseInfo: newEnterprise})
+        res.json({ success: true, message: 'Nhận email thành công', accessToken, enterpriseInfo: newEnterprise })
     } catch (error) {
         console.log(error)
-        res.status(500).json({success: false, message: 'Có gì đó không ổn'})
+        res.status(500).json({ success: false, message: 'Có gì đó không ổn' })
     }
 
 })
 
-router.post('/add', verifyEnterpriseToken, async(req, res) => {
-    const {email, department, phone} = req.body
+router.post('/add', verifyEnterpriseToken, async (req, res) => {
+    const { email, department, phone } = req.body
 
-    if(!email || !department || !phone)
-    return res.status(400).json({success: false, message: 'Please input all fields'})
+    if (!email || !department || !phone)
+        return res.status(400).json({ success: false, message: 'Please input all fields' })
 
-    if(!validateEmail(email))
-    return res.status(400).json({success: false, message: 'Please input another email address'})
+    if (!validateEmail(email))
+        return res.status(400).json({ success: false, message: 'Please input another email address' })
 
-    const staff = await User.findOne({email})
+    const staff = await User.findOne({ email })
 
-        if(staff)
-        return res.status(400).json({success: false, message: 'Email is already exists'})
+    if (staff)
+        return res.status(400).json({ success: false, message: 'Email is already exists' })
 
     try {
         const addStaff = new User({
@@ -66,8 +79,8 @@ router.post('/add', verifyEnterpriseToken, async(req, res) => {
             status: false,
             department,
             phone,
-            enterpriseId : req.enterpriseId,
-            enterpriseName : req.name,
+            enterpriseId: req.enterpriseId,
+            enterpriseName: req.name,
         })
 
         await addStaff.save()
@@ -76,27 +89,27 @@ router.post('/add', verifyEnterpriseToken, async(req, res) => {
 
         sendMail(email, url)
 
-        res.json({success: true, message: 'Nhận email thành công', staffInfo: addStaff})
+        res.json({ success: true, message: 'Nhận email thành công', staffInfo: addStaff })
     } catch (error) {
         console.log(error)
-        res.status(500).json({success: false, message: 'Có gì đó không ổn'})
+        res.status(500).json({ success: false, message: 'Có gì đó không ổn' })
     }
 })
 
-router.get('/getAllEnterprise', async(req, res) => {
+router.get('/getAllEnterprise', async (req, res) => {
     const email = req.query.email;
     const emailQuery = email ? { email: { $regex: new RegExp(email), $options: "i" } } : {};
 
     Enterprise.find(emailQuery)
-    .then(data => {
-        res.send(data);
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while retrieving forms."
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving forms."
+            });
         });
-      });
 })
 
 function validateEmail(email) {
