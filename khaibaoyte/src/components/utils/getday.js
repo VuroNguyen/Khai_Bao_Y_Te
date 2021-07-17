149dbc8e4a845ae416ecb8905b3e1b2e79369287
ollylll97lll <58421531+ollylll97lll@users.noreply.github.com>
2021-07-17 13:09:32 +0700

    Update Task Dang Quoc Anh
    -local storage chạy ở trang form của ng dùng .
    Hoàn thành việc sút ng dùng ra nếu k có token/ token hết hạn.
    Tạo 2 utils mới để lấy ngày trong tuần và tạo routes riêng chỉ ai có token mới vào được
    LƯU Ý:  các route riêng chỉ cần có token. CHƯA XÉT ROLE CỦA TOKEN (vd: ROLE USER, ROLE ENTERPRISE)
    *sẽ update sau khi họp lại

---
+++ khaibaoyte/src/components/utils/getday.js
@@ -0,0 +1,32 @@
+export const getDay = (day) => {
+    switch(day){
+        case 0:{
+            return 'Chủ Nhật'
+            break;
+        }
+        case 1:{
+            return 'Thứ hai'
+            break;
+        }
+        case 2:{
+            return 'Thứ ba'
+            break;
+        }
+        case 3:{
+            return 'Thứ tư'
+            break;
+        }
+        case 4:{
+            return 'Thứ năm'
+            break;
+        }
+        case 5:{
+            return 'Thứ sáu'
+            break;
+        }
+        case 6:{
+            return 'Thứ bảy'
+            break;
+        }
+    }
+}
---
+++ khaibaoyte/src/components/utils/privateroutes.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Redirect, Route } from 'react-router-dom';
+
+export default function PrivateRoutes({component: Component, ...rest}) {
+    const userToken = localStorage.getItem('khaibaoyte');
+    const userInfo = userToken;
+    return (
+        <Route
+        {...rest}
+        render={(props) =>
+          userInfo ? (
+            <Component {...props}></Component>
+          ) : (
+            <Redirect to="/" />
+          )
+        }
+      ></Route>
+    )
+}
