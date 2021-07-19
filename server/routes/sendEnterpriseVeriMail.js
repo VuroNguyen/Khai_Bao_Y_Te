b5d442f59acb688281f2f89f0bbf6539ab0ed45b
nhatlam1995 <nhatlam1695@gmail.com>
2021-07-19 14:37:18 +0700

    mail fix

--- server/routes/sendEnterpriseVeriMail.js
+++ server/routes/sendEnterpriseVeriMail.js
@@ -44,7 +44,7 @@ const sendEnterpriseVeriMail = (to, url, token) => {
         to: to,
         subject: 'Yêu cầu xác thực email',
         html: `
-        <p>Xin chào,</p>
+        <p>Xin chào ${to},</p>
         <p>Cảm ơn bạn đã truy cập <strong>Hệ thống Khai báo y tế dành cho Doanh nghiệp</strong></p>
         <p>Bạn vui lòng chọn <strong>Xác Nhận</strong> để bắt đầu đăng ký thông tin Doanh nghiệp và quản lý nhân viên</p>
         <p>&nbsp;</p>
