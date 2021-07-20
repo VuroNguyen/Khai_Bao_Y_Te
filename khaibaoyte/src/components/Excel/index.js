89ab82f95a3828eececc0ee109707a85550b2662
nhatlam1995 <nhatlam1695@gmail.com>
2021-07-20 16:49:38 +0700

    Excel export

---
+++ khaibaoyte/src/components/Excel/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import * as FileSaver from "file-saver";
+import { Button } from 'reactstrap'
+import * as XLSX from "xlsx";
+
+export const ExportToExcel = ({ apiData, fileName }) => {
+    const fileType =
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
+    const fileExtension = ".xlsx";
+
+    const exportToCSV = (apiData, fileName) => {
+        const ws = XLSX.utils.json_to_sheet(apiData);
+        const wb = { Sheets: { data: ws }, SheetNames: ["data"] };
+        const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
+        const data = new Blob([excelBuffer], { type: fileType });
+        FileSaver.saveAs(data, fileName + fileExtension);
+    };
+
+    return (
+        <Button outline color="info" onClick={(e) => exportToCSV(apiData, fileName)}>Xuất ra file Excel</Button>
+    );
+};
